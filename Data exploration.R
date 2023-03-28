69f2b97fa75887910bc023e6be4c1db1a9e1c2b1
kierstinacuna <kierstin.acuna@gmail.com>
2023-03-28 11:23:14 -0700

    More tidying

--- Data exploration.R
+++ Data exploration.R
@@ -10,7 +10,7 @@ str(forage)
 str(weather)
 
 # Read in dates as dates, change column names
-test <- weather %>% 
+weather <- weather %>% 
   mutate(Date = ym(Date),
          Year = year(Date),
          Month = month(Date)) %>% 
@@ -19,3 +19,29 @@ test <- weather %>%
          tmean = tmean..degrees.F.) %>% 
   relocate(Year, .after = Date) %>% 
   relocate(Month, .after = Year)
+
+forage <- forage %>% 
+  rename(Year = year,
+         Forage = lbs_per_acre)
+
+# Merge data together
+data <- merge(weather, forage, by="Year", all.x = T)
+
+data <- data %>% 
+  arrange(Year, Month) %>% 
+  relocate(Date, .before = Year)
+
+write.csv(data, "Full data.csv")
+
+# Make histograms, explore relationships
+panel.hist <- function(x, ...)
+{
+  usr <- par("usr")
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col = "cyan", ...)
+}
+
+pairs(data[,2:7], diag.panel = panel.hist)
