8db4a54ba0f3f5fae1165015ef5116da84c8dcb0
sethjromero <112140012+sethjromero@users.noreply.github.com>
2023-04-04 11:37:28 -0700

    updating progress

---
+++ forageDataCleaning.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+setwd("~/Documents/GitHub/CourseResources/EcologicalForecasting/challenge1/")
+
+weather <- read.csv("../data/weather data - PRISM_ppt_tmin_tmean_stable_4km_193501_201801_37.1019_-119.7324.csv") %>%
+  mutate(date = ymd(paste(as.character(Date), "-01", sep = ""))) %>%
+  rename(month = Date,
+         pptIn = ppt..inches.,
+         tmin = tmin..degrees.F.,
+         tmean = tmean..degrees.F.)
+
+forage <- read.csv("../data/Forage Data - Sheet1.csv") %>%
+  mutate(date = mdy(paste("05-01-", as.character(year), sep = "")),
+         month = paste(as.character(year), "-05", sep = ""))
+
+modelIn <- weather %>%
+  left_join(forage, by = "month") %>%
+  select(-date.x, -date.y) %>%
+  mutate(date = ymd(paste(month, "-01", sep = "")),
+         month = month(date),
+         year = year(date),
+         winterYear = ifelse(month == 1, year-1, year),
+         winterMonth = ifelse(month == 1, 12, month-1)) %>%
+  select(pptIn, tmin, tmean, year, winterMonth, winterYear) %>%
+  mutate(year = winterYear + 1) %>%
+  left_join(forage, by = "year") %>%
+  select(-date, -month) %>%
+  mutate(percentile = cume_dist(lbs_per_acre)) %>%
+  pivot_longer(cols = c(colnames(.[1:3])),
+               names_to = "variable",
+               values_to = "data") %>%
+  mutate(label = paste(variable, winterMonth, sep = "_")) %>%
+  select(-variable, -winterMonth) %>%
+  pivot_wider(names_from = "label", values_from = "data")
